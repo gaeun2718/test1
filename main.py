32b0b52f7eb1523aa60d2ec70572bf7552a8428f
gaeun2718 <sudo2630321@sonline20.sen.go.kr>
2026-07-13 10:44:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,17 @@ st.write("노직도 바보임")
 지역 = st.selectbox("지역을 골라 보세요", ["서울", "양평", "부산"])
 st.write("당신이 고른 지역:", 지역)
 
-숫자 = st.slider("좋아하는 숫자", 0, 100)
-st.write("고른 숫자:", 숫자)
  
-if st.button("풍선 날리기"):
-	st.balloons()
-
- 
-tab1, tab2 = st.tabs(["첫 번째 탭", "두 번째 탭"])
+tab1, tab2, tab3 = st.tabs(["첫 번째 탭", "두 번째 탭", "세 번째 탭"])
  
 with tab1:
 	st.write("여기는 첫 번째 탭이에요.")
+	if st.button("풍선 날리기"):st.balloons()
  
 with tab2:
 	숫자 = st.slider("숫자를 골라보세요", 0, 100)
+	st.write("고른 숫자:", 숫자)
+
+with tab3:
+	숫자 = st.slider("좋아하는 숫자", 0, 100)
 	st.write("고른 숫자:", 숫자)
