616fdf42b8bb6ca4733d1e0780bdf4d116c96623
gaeun2718 <sudo2630321@sonline20.sen.go.kr>
2026-07-13 11:05:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -62,12 +62,18 @@ st.line_chart(temp_df.set_index("일시"))
 st.subheader("시각별 평균 기온차 (서울 - 양평)")
 temp_df["시"] = temp_df["일시"].dt.hour
 temp_df["기온차"] = temp_df["서울"] - temp_df["양평"]
-hourly_diff = temp_df.groupby("시")["기온차"].mean()
+
+hourly_diff = temp_df.groupby("시")["기온차"].mean().reset_index()
+hourly_diff = hourly_diff.set_index("시")
+
 st.bar_chart(hourly_diff)
 
 st.subheader("월별 평균 기온차 (서울 - 양평)")
 temp_df["월"] = temp_df["일시"].dt.month
-monthly_diff = temp_df.groupby("월")["기온차"].mean()
+
+monthly_diff = temp_df.groupby("월")["기온차"].mean().reset_index()
+monthly_diff = monthly_diff.set_index("월")
+
 st.bar_chart(monthly_diff)
 ```
 
@@ -79,22 +85,29 @@ st.bar_chart(monthly_diff)
 
 with tab2:
 st.subheader("기온과 전력수요 관계 (산점도)")
-st.scatter_chart(
-power_df.rename(columns={"서울": "x", "전력수요": "y"})[["x", "y"]]
-)
 
 ```
+scatter_df = power_df[["서울", "전력수요"]].dropna()
+st.scatter_chart(scatter_df)
+
 st.subheader("기온 구간별 평균 전력수요")
 
-# 기온 구간 나누기 (5도 단위)
-bins = range(-20, 45, 5)
+bins = list(range(-20, 45, 5))
 power_df["기온구간"] = pd.cut(power_df["서울"], bins=bins)
 
-temp_power = power_df.groupby("기온구간")["전력수요"].mean()
+temp_power = power_df.groupby("기온구간")["전력수요"].mean().reset_index()
+
+# 문자열로 변환 (이거 중요)
+temp_power["기온구간"] = temp_power["기온구간"].astype(str)
+temp_power = temp_power.set_index("기온구간")
+
 st.bar_chart(temp_power)
 
 st.subheader("월별 평균 전력수요")
+
 power_df["월"] = power_df["일시"].dt.month
-monthly_power = power_df.groupby("월")["전력수요"].mean()
+monthly_power = power_df.groupby("월")["전력수요"].mean().reset_index()
+monthly_power = monthly_power.set_index("월")
+
 st.bar_chart(monthly_power)
 ```
